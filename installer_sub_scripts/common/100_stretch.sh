6803dc19d0ff992a6b67e2b08ec19a4a0634a89e
emrah <emrah.com@gmail.com>
2021-07-27 22:23:09 +0300

    refactor: updated lxc server

--- installer_sub_scripts/common/100_stretch.sh
+++ installer_sub_scripts/common/100_stretch.sh
@@ -63,7 +63,7 @@ set -e
 # create the new one
 lxc-create -n $MACH -t download -P /var/lib/lxc/ -- \
     -d debian -r stretch -a $ARCH \
-    --server "us.images.linuxcontainers.org" \
+    --server "uk.images.linuxcontainers.org" \
     --keyserver "keyserver.ubuntu.com"
 
 # shared directories
